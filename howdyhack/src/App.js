4a1b7e0d943667f0194a5964552b37d32a6c5180
Akshay Belhe <abelhe8900@gmail.com>
2022-10-01 16:26:19 -0500

    d

--- howdyhack/src/App.js
+++ howdyhack/src/App.js
@@ -1,6 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+const response = await openai.createCompletion({
+  model: "text-davinci-002",
+  prompt: "Say this is a test",
+  temperature: 0,
+  max_tokens: 6,
+});
+
 function App() {
   return (
     <div className="app name">
