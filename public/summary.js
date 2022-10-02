539e71b715cb5e321d2f5c5d9c124ecb700b8aa6
yoosh2842 <thereqltimel@gmail.com>
2022-10-02 05:53:37 -0500

    pls work

---
+++ public/summary.js
@@ -0,0 +1,17 @@
+// import {Configuration, OpenAIApi} from "./node_modules/openai";
+//         async function myFunction() {
+          
+//           var x = document.getElementById("fname").value;
+
+//         const configuration = new Configuration({
+//           apiKey: "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"
+//         });
+//         const openai = new OpenAIApi(configuration);
+//         const completion = await openai.createCompletion({
+//           model: "text-ada-001",
+//           prompt: x,
+//         });
+//         console.log(completion.data.choices[0].text);
+
+//         }
+        
