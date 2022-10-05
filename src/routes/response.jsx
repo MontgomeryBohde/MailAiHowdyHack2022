7c5b10f492e9f3afcf8d8638a52fea91e51bcbca
MontgomeryBohde <mbohde2015@tamu.edu>
2022-10-05 10:26:38 -0500

    initial changes for response page

--- src/routes/response.jsx
+++ src/routes/response.jsx
@@ -1,7 +1,18 @@
+import "./response.css";
+
 export default function Response() {
     return (
         <>
-        <h1>Respond to Text</h1>
+            <div className="main">
+                <div id="centertxt">
+                    <a href="/"><h3 className="title">mail.AI</h3></a>  
+                </div>                                         
+                <h4><span className="text_2">Input a text you would like to generate a response to</span></h4>
+                <textArea className="inputME" type="text" id="fname" name="fname"> </textArea>
+                <input className="responseButton" type="submit" value="Create Response" onclick="myFunction()"/>
+                <output className="outputME" id="outputMeee"></output>
+                <textArea className="outputME" id="outputME"> </textArea> 
+            </div>
         </>
     );
 }
