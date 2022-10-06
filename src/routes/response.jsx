562de59805d23fb18265f56f4e68eedd89a0b8c9
MontgomeryBohde <mbohde2015@tamu.edu>
2022-10-05 19:09:47 -0500

    improve response page

--- src/routes/response.jsx
+++ src/routes/response.jsx
@@ -4,12 +4,12 @@ export default function Response() {
     return (
         <>
             <div className="main">
-                <div id="centertxt">
+                <div className="centertxt">
                     <a href="/"><h3 className="title">mail.AI</h3></a>  
+                    <input className="responseButton" type="submit" value="Create Response" onclick="myFunction()"/>
+                    <h4><span className="text_2">Input a text you would like to generate a response to</span></h4>
                 </div>                                         
-                <h4><span className="text_2">Input a text you would like to generate a response to</span></h4>
                 <textArea className="inputME" type="text" id="fname" name="fname"> </textArea>
-                <input className="responseButton" type="submit" value="Create Response" onclick="myFunction()"/>
                 <output className="outputME" id="outputMeee"></output>
                 <textArea className="outputME" id="outputME"> </textArea> 
             </div>
