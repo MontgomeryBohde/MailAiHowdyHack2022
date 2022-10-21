4482d64dd87774a14e43d51e1af76760af1f5b1c
Akshay Belhe <abelhe8900@gmail.com>
2022-10-21 01:01:19 -0500

    fixed typewriter

---
+++ src/routes/summary/summary.jsx
@@ -0,0 +1,15 @@
+import "./summary.css"
+
+export default function Summary() {
+    return (
+        <>
+            <div className= "summarypage">
+                <a href="/"><h3 class="logo">mail.AI</h3></a>
+                <h4><span class="text_2">Input a long piece of text you would like to summarize</span></h4>
+          
+            </div>     
+        </>
+
+
+    );
+}
