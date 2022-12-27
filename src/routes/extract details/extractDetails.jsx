ff04d62bc51a4c35d361eed1411fe92b2fa262d6
yoosh2842 <thereqltimel@gmail.com>
2022-12-26 23:35:06 -0600

    idk what this is but it's probably important

--- src/routes/extract details/extractDetails.jsx
+++ src/routes/extract details/extractDetails.jsx
@@ -4,7 +4,7 @@ export default function ExtractDetails() {
             <div className="main">
                 <div className="centertxt">
                     <a href="/"><h3 className="title">mail.AI</h3></a>  
-                    <input className="responseButton" type="submit" value="Create Summary" onclick="myFunction()"/>
+                    <input className="responseButton" type="submit" value="Extract Details" onclick="myFunction()"/>
                     <h4><span className="text_2">Input a text you would like to extract details from</span></h4>
                 </div>                                         
                 <textArea className="inputME" type="text" id="fname" name="fname"> </textArea>
