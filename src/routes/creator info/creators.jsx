04bd265dd076b786fc515ded0b7dc606007802bd
yoosh2842 <thereqltimel@gmail.com>
2022-12-26 23:47:34 -0600

    idk what this is but it's probably important

--- src/routes/creator info/creators.jsx
+++ src/routes/creator info/creators.jsx
@@ -7,10 +7,10 @@ export default function Creators() {
     <>
       <div className= "main">
         <h1 className="Title">mail.AI was created at HowdyHack2022 By:</h1>
-        <h2 className="Monte">Montgomery Bohde, Full Stack God <a href="https://www.linkedin.com/in/montgomery-bohde/" target= "_blank" style={{ textDecoration: 'none' }}><FontAwesomeIcon icon={faLinkedin} /></a></h2>
-        <h2 className='Aayush'>Aayush Garg, Frontend Engineer <a href="https://www.linkedin.com/in/aayushg1414/" target= "_blank" style={{ textDecoration: 'none' }}><FontAwesomeIcon icon={faLinkedin} /></a></h2>
-        <h2 className="Akshay">Akshay Belhe, UI/UX Specialist <a href="https://www.linkedin.com/in/akshaybelhe/" target= "_blank" style={{ textDecoration: 'none' }}><FontAwesomeIcon icon={faLinkedin} /></a></h2>
-        <h2 className="Aditya">Aditya Nambi (Flu) <a href="https://www.linkedin.com/in/adityanambi/" target= "_blank" style={{ textDecoration: 'none' }}><FontAwesomeIcon icon={faLinkedin} /></a></h2>
+        <h2 className="Monte">Montgomery Bohde, Full Stack God <a href="https://www.linkedin.com/in/montgomery-bohde/" target= "_blank" style={{ textDecoration: 'none' }} className='linkedin-icon'><FontAwesomeIcon icon={faLinkedin} /></a></h2>
+        <h2 className='Aayush'>Aayush Garg, Frontend Engineer <a href="https://www.linkedin.com/in/aayushg1414/" target= "_blank" style={{ textDecoration: 'none' }} className='linkedin-icon'><FontAwesomeIcon icon={faLinkedin} /></a></h2>
+        <h2 className="Akshay">Akshay Belhe, UI/UX Specialist <a href="https://www.linkedin.com/in/akshaybelhe/" target= "_blank" style={{ textDecoration: 'none' }} className='linkedin-icon'><FontAwesomeIcon icon={faLinkedin} /></a></h2>
+        <h2 className="Aditya">Aditya Nambi (Flu) <a href="https://www.linkedin.com/in/adityanambi/" target= "_blank" style={{ textDecoration: 'none' }} className='linkedin-icon'><FontAwesomeIcon icon={faLinkedin} /></a></h2>
       </div>
     </>
   );
